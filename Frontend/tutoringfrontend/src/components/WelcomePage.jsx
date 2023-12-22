af9951390408a5afea9d423d4b6f0264045aa2c5
Imad Buljic <imad.buljic.22@size.ba>
2023-12-22 10:43:46 +0100

    Popravljen nacin obrade jwta, dodan cookie sistem rada, dodana provjera na welcomePage, postavljena bolja cors politika

--- Frontend/tutoringfrontend/src/components/WelcomePage.jsx
+++ Frontend/tutoringfrontend/src/components/WelcomePage.jsx
@@ -6,6 +6,33 @@ const WelcomePage = () => {
    // const token = localStorage.getItem('token'); ne koristimo vise localstorage nego cookies
 const [username,setUsername]=useState('');
 
+    // useEffect(() => {
+    //     const getUsername = async () => {
+    //         try {
+    //             const response = await fetch('http://localhost:8080/api/welcomePage', {
+    //                 method: 'GET',
+    //                 credentials: 'include', // Šalje cookie-e uz zahtjev
+    //             });
+    //
+    //             // Logovanje Set-Cookie headera iz odgovora, ako je dostupan
+    //             console.log('Set-Cookie header:', response.headers.get('Set-Cookie'));
+    //
+    //             if (!response.ok) {
+    //                 console.log("NIJE OK");
+    //                 throw new Error(`Problem with fetch: ${response.statusText}`);
+    //             }
+    //
+    //             const data = await response.text(); // Pretpostavimo da server vraća JSON
+    //             console.log("OVO SE POSTAVLJA", data);
+    //             setUsername(data.username); // Pretpostavimo da je 'username' ključ u JSON odgovoru
+    //         } catch (error) {
+    //             console.error("Error sa fetchom", error);
+    //         }
+    //     };
+    //
+    //     getUsername();
+    // }, []);
+
     useEffect(() => {
         const getUsername = async () => {
             try {
