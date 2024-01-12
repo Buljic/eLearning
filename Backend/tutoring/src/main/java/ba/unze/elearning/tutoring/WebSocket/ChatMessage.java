a8af533d92616c24b759ebdaacd910468f24107c
Imad Buljic <imad.buljic.22@size.ba>
2024-01-12 22:52:52 +0100

    Pocetak implementacije kontrollera za chat

---
+++ Backend/tutoring/src/main/java/ba/unze/elearning/tutoring/WebSocket/ChatMessage.java
@@ -0,0 +1,16 @@
+package ba.unze.elearning.tutoring.WebSocket;
+
+public class ChatMessage
+{
+    String message;
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }
+}
