3c4f472ed6c752c041bc358b6b5e5e51c6e83187
Buljic <imad.buljic.22@size.ba>
2024-01-20 09:43:06 +0100

    Dodane funkcije za slanje i primanje poruka za ws

--- Frontend/tutoringfrontend/src/minicomponents/Chat.jsx
+++ Frontend/tutoringfrontend/src/minicomponents/Chat.jsx
@@ -1,45 +1,75 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 
 const Chat=({chatId,isGroupChat})=>{//ako je group chat onda proslijedujemo groupchatid a inace samo id te osobe
+
     const storedUser=sessionStorage.getItem('myUser');
     const myUser=JSON.parse(storedUser);
-    if(!isGroupChat)
-    {
-        useEffect(()=>{
-            // const websocket=new WebSocket('ws://localhost:8080/chat');
-            const socket=new SockJS('http://localhost:8080/api/chatTo');//navodno znati ce se da je ovdje veza ta
-            const stompClient=Stomp.over(socket);//koristimo socket iznad kao argument za ovaj stompClient
 
-            stompClient.connect({},function(frame){
-                console.log('Povezano:'+frame);
+    const stompClient=useRef(null);
+    const[ourEndpoint,setOurEndpoint]=useState('');
 
-                if(myUser.id<chatId/*parseInt(objectUser)*/)
-                {
-                    stompClient.subscribe('/queue/'+myUser.id.toString()+'/'+/*objectUser.*/chatId.toString(),function (messageOutput) {
+    // if(!isGroupChat) //TODO Postavi i za groupChat
 
-                        // appendMessage(messageOutput.body,chatId); TODO ovdje kao treba da se filtrira cija je poruka
+    useEffect(() => {
+        // const websocket=new WebSocket('ws://localhost:8080/chat');
+        const socket = new SockJS('http://localhost:8080/api/chatTo');//navodno znati ce se da je ovdje veza ta
+        //const stompClient = Stomp.over(socket);//koristimo socket iznad kao argument za ovaj stompClient
+        stompClient.current=Stomp.over(socket);
 
-                    });//todo finish
-                }else {
-                    stompClient.subscribe('/queue/'+chatId.toString()+'/'+myUser.id.toString(),function (messageOutput) {
+        stompClient.current.connect({}, function (frame) {
+            console.log('Povezano:' + frame);
 
-                    });
-
-                }
-            });
-            function appendMessage(messageBody, sender) {
-
+            if (myUser.id < chatId)
+            {
+                setOurEndpoint(  /*'/queue/'*/ + myUser.id.toString() + '/' +/*objectUser.*/chatId.toString());
+            }//TODO popravi endpointe tj njihvo mapiranje
+            else
+            {
+                setOurEndpoint(/*'/queue/' +*/ chatId.toString() + '/' + myUser.id.toString());
             }
 
-        },[]);
+            stompClient.current.subscribe(ourEndpoint, (messageOutput) => {
+                appendMessage(messageOutput.body);
+            });
+        });
+
+        // const sendMessage=()=>
+
+        function appendMessage(messageBody)
+        {
+            let chatBox = document.getElementById('chatBox');
+            let messageElement = document.createElement('div');//da fazon kreira div za jednu neku poruku
+
+            messageElement.innerText = messageBody.message;//u taj novi nas div stavlja poruku tu koju joj proslijedimo
+
+            if (messageBody.receiver === chatId)//ako mi saljemo poruku nekome
+            {
+                messageElement.className = 'sent-message';
+            }
+            else
+            {
+                messageElement.className = 'received-message';
+            }
+            chatBox.appendChild(messageElement);//da u onaj nas chatBox element appenda jos jedan element
+        }
+
+        //TODO dodaj cleanup funkciju
+    }, [ourEndpoint]);
+    function sendMessage()
+    {
+        let messageElement = document.getElementById('messageInput');
+        let messageText = messageElement.value; //value se koristi za input field a ne innerText(on za <p> i sl)
+        if (messageText.trim() !== '')//da nije prazno
+        {
+            console.log(messageText+"OVO JE PORUKA");
+            stompClient.current.send(ourEndpoint, {}, JSON.stringify(messageText));
+            messageElement.value = '';
+        }
     }
 
-//TODO BOLJI ce pristup biti napraviti algoritam za kreiranje idija ili jednostavno mozda auto increment
-// umjesto pravljenja dinamickih endpointa jer cemo tako moci koristiti jedan endpoint za 2 usera a u isto
-// vrijeme moci jednostavno to praviti jer ce sve biti u nekoj tabeli spremljeno
 
     return (
         <div>
@@ -49,17 +79,10 @@ const Chat=({chatId,isGroupChat})=>{//ako je group chat onda proslijedujemo grou
             </div>
             {/*TODO POSTAVI KONFIGURACIJU POSTO RADIMO S VITEOM*/}
             <input type="text" id="messageInput"></input>
+            <button type="submit" onClick={()=>sendMessage()}>Slanje</button>
 
         </div>
     );
 }
 
-
-
 export default Chat;
-
-
-
-
-
-
