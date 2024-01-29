a666800fe64e6fbfd167cb1efdaa3043e8d13755
Imad Buljic <imad.buljic.22@size.ba>
2024-01-29 21:33:39 +0100

    Popravljeno real time prikazivanje poruka

--- Backend/tutoring/src/main/java/ba/unze/elearning/tutoring/WebSocket/ChatController.java
+++ Backend/tutoring/src/main/java/ba/unze/elearning/tutoring/WebSocket/ChatController.java
@@ -1,5 +1,6 @@
 package ba.unze.elearning.tutoring.WebSocket;
 
+import ba.unze.elearning.tutoring.DTOs.GenericDTO;
 import ba.unze.elearning.tutoring.Repositories.DirectMessageRepository;
 import ba.unze.elearning.tutoring.Repositories.UserRepository;
 import ba.unze.elearning.tutoring.Security.JwtUtil;
@@ -13,6 +14,8 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller //ne treba nam restcontrollerovdje jer koristimo @Messagemappiong
 public class ChatController
@@ -36,6 +39,13 @@ public class ChatController
     @GetMapping ("/api/{user1}/{user2}/getOldDirectMessages")
     public ResponseEntity<?> getOldDMs(@PathVariable String user1, @PathVariable String user2)
     {
+        List<GenericDTO> dto= messageService.getOldDMs(Long.parseLong(user2), Long.parseLong(user1));
+       // System.out.println(dto);
+        System.out.println("------ELEMENT------");
+        for(GenericDTO i:dto)
+        {
+            System.out.println(i);
+        }
        return ResponseEntity.status(HttpStatus.OK).body( messageService.getOldDMs(Long.parseLong(user2), Long.parseLong(user1)));
     }
 
