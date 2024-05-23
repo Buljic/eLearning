445ce2bbe380be1787b88c7ce93afa450547afbe
Imad Buljic <imad.buljic.22@size.ba>
2024-05-23 20:08:47 +0200

    Added GroupResult helper component

---
+++ Frontend/tutoringfrontend/src/minicomponents/GroupResult.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const GroupResults = ({ isSearching, groups }) => {
+    return (
+        <div style={{ flex: 2 }}>
+            <h1>Rezultati Pretrage</h1>
+            {isSearching ? (
+                <ul>
+                    {groups.map((group, index) => (
+                        <li key={index}>
+                            <h3>{group.group_name}</h3>
+                            <p>Opis: {group.description}</p>
+                            <p>Datum početka: {group.start_date}</p>
+                            <p>Datum završetka: {group.end_date}</p>
+                            <p>Sati po sedmici: {group.hours_per_week}</p>
+                            <p>Cijena: {group.price} BAM</p>
+                            <p>Maksimalan broj studenata: {group.max_students}</p>
+                            <p>Topic: {group.topic}</p>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>Nema rezultata pretrage.</p>
+            )}
+        </div>
+    );
+};
+
+export default GroupResults;
