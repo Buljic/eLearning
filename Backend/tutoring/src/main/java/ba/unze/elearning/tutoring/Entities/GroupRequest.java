9b32ad085b2cb2d4123081c2c79863171ffef1d1
Imad Buljic <imad.buljic.22@size.ba>
2024-05-25 19:31:40 +0200

    Added the functionality to display paginated requests for group entry for profesor users , added their ability to accept or reject them , added automatic reject of timed out requests

--- Backend/tutoring/src/main/java/ba/unze/elearning/tutoring/Entities/GroupRequest.java
+++ Backend/tutoring/src/main/java/ba/unze/elearning/tutoring/Entities/GroupRequest.java
@@ -27,6 +27,32 @@ public class GroupRequest {
     @Enumerated(EnumType.STRING)
     private RequestStatus status;
 
+    @Transient
+    private String groupName;
+
+    @Transient
+    private String username;
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getGroupName()
+    {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName)
+    {
+        this.groupName = groupName;
+    }
+
     public GroupRequestId getId()
     {
         return id;
