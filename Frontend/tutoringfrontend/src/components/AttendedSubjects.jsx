768f898c7c191a3d8c20546f32e580edcb0db1e0
Imad Buljic <imad.buljic.22@size.ba>
2024-05-29 13:33:13 +0200

    Fixed display of attended courses for Student users

--- Frontend/tutoringfrontend/src/components/AttendedSubjects.jsx
+++ Frontend/tutoringfrontend/src/components/AttendedSubjects.jsx
@@ -1,57 +1,57 @@
-import React, {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-const AttendedSubjects=()=>{
+const AttendedSubjects = () => {
+    const storedUser = sessionStorage.getItem('myUser');
+    const myUser = JSON.parse(storedUser);
 
-    const storedUser=sessionStorage.getItem('myUser');
-    const myUser=JSON.parse(storedUser);
+    const [attendedGroups, setAttendedGroups] = useState([]);
 
-    const [attendedGroups,setAttendedGroups]=useState([]);
-
-    useEffect(()=>{
-        const getAttendedSubjects=async()=>{
-            const response=await fetch(`http://localhost:8080/api/getAttendedGroups?userId=${myUser.id}`,{
-                method:'GET',
-                credentials:'include',
-                headers:{
-                    'Content-Type':'application/json',
-                }
-            });
-            if (!response.ok)
-            {
-                throw new Error('Problem s fetchanjem attended groups');
-            }
-            else{
-                const data= await response.json();
-                setAttendedGroups(data);
-                attendedGroups.forEach(subject=>{
-                    console.log('ovo je grupa'+ subject.group_id);
+    useEffect(() => {
+        const getAttendedSubjects = async () => {
+            try {
+                const response = await fetch(`http://localhost:8080/api/getAttendedGroups?userId=${myUser.id}`, {
+                    method: 'GET',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
                 });
+                if (!response.ok) {
+                    throw new Error('Problem s fetchanjem attended groups');
+                } else {
+                    const data = await response.json();
+                    setAttendedGroups(data);
+                    data.forEach(subject => {
+                        console.log('Ovo je grupa ' + subject.group_id);
+                    });
+                }
+            } catch (error) {
+                console.error(error);
             }
         }
         getAttendedSubjects();
-    },[]);
-    if(!attendedGroups)
-    {
-        return <h1>Ucitavanje ...</h1>
+    }, [myUser.id]);
+
+    if (!attendedGroups.length) {
+        return <h1>Učitavanje ...</h1>
     }
-    return(
-      <div>
-          <h1>Pohađani kursevi</h1>
 
-          <br/>
-          {attendedGroups && (
-              <ul id="attendedGroups">
-                  {attendedGroups.map((group, index)=>(
-                      <li key={index} id={group}>
-                           <Link to={`/chatGroup/${group.group_id}`}>  {group.group_name}</Link>
-                      </li>
-                  ))}
-              </ul>
-          )}
-      </div>
-
+    return (
+        <div>
+            <h1>Pohađani kursevi</h1>
+            <br />
+            {attendedGroups.length > 0 && (
+                <ul id="attendedGroups">
+                    {attendedGroups.map((group, index) => (
+                        <li key={index} id={group.group_id}>
+                            <Link to={`/chatGroup/${group.group_id}`}>{group.group_name}</Link>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
     );
-
 }
+
 export default AttendedSubjects;
