0dae2180ac83f9ea973a425f940ea428aeccabec
Imad Buljic <imad.buljic.22@size.ba>
2024-05-31 11:50:56 +0200

    Added asignment submission entity

--- Backend/tutoring/src/main/java/ba/unze/elearning/tutoring/Entities/Assignment.java
+++ Backend/tutoring/src/main/java/ba/unze/elearning/tutoring/Entities/Assignment.java
@@ -22,5 +22,83 @@ public class Assignment {
     @OneToMany(mappedBy = "assignment", cascade = CascadeType.ALL)
     private List<AssignmentSubmission> submissions;
 
+    public Long getId()
+    {
+        return id;
+    }
 
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate()
+    {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate)
+    {
+        this.dueDate = dueDate;
+    }
+
+    public Integer getPoints()
+    {
+        return points;
+    }
+
+    public void setPoints(Integer points)
+    {
+        this.points = points;
+    }
+
+    public String getImageUrl()
+    {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl)
+    {
+        this.imageUrl = imageUrl;
+    }
+
+    public Group getGroup()
+    {
+        return group;
+    }
+
+    public void setGroup(Group group)
+    {
+        this.group = group;
+    }
+
+    public List<AssignmentSubmission> getSubmissions()
+    {
+        return submissions;
+    }
+
+    public void setSubmissions(List<AssignmentSubmission> submissions)
+    {
+        this.submissions = submissions;
+    }
 }
