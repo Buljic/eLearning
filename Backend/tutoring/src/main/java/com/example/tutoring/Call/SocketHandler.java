aa34a2538734307dd49dbdda72de44988eb28f81
Imad Buljic <imad.buljic.22@size.ba>
2024-06-03 14:42:27 +0200

    Dodavanje SocketHandler i SocketIOConfig klasa

---
+++ Backend/tutoring/src/main/java/com/example/tutoring/Call/SocketHandler.java
@@ -0,0 +1,32 @@
+package com.example.tutoring.Call;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.TextMessage;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class SocketHandler extends TextWebSocketHandler {
+
+    private static CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())) {
+                webSocketSession.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+}
+
